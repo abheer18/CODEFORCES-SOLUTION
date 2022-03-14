4b01569f6e6f93c42a438f4d726bb769fdc36710
ABHEER MEHROTRA <72267713+abheer18@users.noreply.github.com>
2022-03-14 10:39:53 +0530

    Rating 800

---
+++ A_Translation.py
@@ -0,0 +1,13 @@
+# word1 = list(input().split())
+# word2 = list(input().split())
+# word1.reverse()
+# if(word1 == word2):
+#     print("Yes")
+# else:
+#     print("No")
+s = input()
+y = input()
+if s[::-1] == y:
+	print ("YES")
+else:
+	print ("NO")
